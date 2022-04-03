013b66bab2a4e8320f12bb5c6cd9a88702e5fc74
Pooja Sree <you@example.com>
2022-04-03 23:04:41 +0530

    UC2validatecontact

--- addressbook.js
+++ addressbook.js
@@ -28,6 +28,93 @@ class AddressBook{
         +this.city+","+"address: "+this.address+","+"state: "+this.state+","+"zip: "+this.zip
         +","+"mobnum: "+this.mobnum+","+"email: "+this.email;
     }
+
+
+getfirstName()
+{ 
+    return this._firstName
 }
-let addressbook = new AddressBook("varsha","sree","hyderabad","shankar nagar","telangana",500013,9946582187,"varshasree8@gmail.com")
+
+setfirstName(firstName) 
+{
+    let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+    if(nameRegex.test(firstName))
+        this._firstName = firstName;
+    else throw "Incorrect First Name";
+}
+getlastName()
+{ return this._lastName}
+setlastName(lastName)
+{
+    let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+    if(nameRegex.test(lastName))
+        this._lastName = lastName
+    else throw "Incorrect Last Name"
+}
+getaddress()
+{ return this._address}
+setaddress(address)
+{
+    let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+    if(regex.test(address))
+        this._address = address
+    else throw "Incorrect Address"
+}
+getcity()
+ { return this._city}
+setcity(city)
+{
+    let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+    if(regex.test(city))
+        this._city = city
+    else throw "Incorrect City"
+}
+getstate()
+{ return this._state}
+setstate(state)
+{
+    let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+    if(regex.test(state))
+        this._state = state
+    else throw "Incorrect State"
+}
+getzip()
+{ return this._zip}
+setzip(zip)
+{
+
+    let pinRegex = RegExp('^[0-9]{3}[\\s]{0,1}[0-9]{3}$')
+    if( pinRegex.test(zip)){
+
+        this._zip = zip
+    }
+    else throw "Incorrect Zip"
+}
+getphoneNo()
+{ return this._phoneNo}
+setphoneNo(phoneNo)
+{
+    let regex = RegExp('[0-9]{2}\\s[0-9]{10}')
+    if(regex.test(phoneNo))
+        this._phoneNo = phoneNo
+    else throw "Incorrect Phone Number"
+}
+getemail()
+{ return this._email}
+setemail(email)
+{
+    let regex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
+    if(regex.test(email))
+        this._email = email
+    else throw "Incorrect Email"
+}
+
+}
+
+try{
+let addressbook = new AddressBook("varsha","sree","shankarnager","hyderabad","telangana",500013,9946582187,"varshasree8@gmail.com")
 console.log(addressbook.toString());
+}
+catch(e){
+console.log(e)
+}
